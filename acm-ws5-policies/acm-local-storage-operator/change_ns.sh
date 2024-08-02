49ee0fb5f95907a70ba6a1e6395cdee466a26ec3
Andreas Entgelmeier <entgelme@de.ibm.com>
2024-08-02 16:47:11 +0200

    zu viel kopiert

--- acm-ws5-policies/acm-local-storage-operator/change_ns.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -x
-
-sed -i '' -e 's/1-openshift-storage/openshift-storage/g' openshift-storage-ns.yml
-sed -i '' -e 's/1-openshift-storage/openshift-storage/g' openshift-storage-operatorgroup.yml
-sed -i '' -e 's/1-openshift-storage/openshift-storage/g' odf-operator-subs.yml
-sed -i '' -e 's/1-openshift-storage/openshift-storage/g' ocs-storagecluster.yml
