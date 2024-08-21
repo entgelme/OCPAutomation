e731eace05d75daa3de88cddb972b731f6a1eebe
Andreas Entgelmeier <entgelme@de.ibm.com>
2024-08-21 16:04:06 +0200

    CLUSTER_INIT_BUNDLE_FILE as parameter

--- ACS/roxctl-setup.sh
+++ ACS/roxctl-setup.sh
@@ -5,10 +5,11 @@
 MC_APIURL=$1
 MC_APITOKEN=$2
 ROXCTL_ACCESS_TOKEN_FILE=$3
+CLUSTER_INIT_BUNDLE_FILE=$4
 
-if [ "$#" -ne 3 ]; then
+if [ "$#" -ne 4 ]; then
     echo "Illegal number of parameters"
-    echo -e "Usage $0 <MC API URL incl. port> <MC API Token> <path-to-roxctl-access-token-file>\n"
+    echo -e "Usage $0 <MC API URL incl. port> <MC API Token> <path-to-roxctl-access-token-file> <cluster init bundle file>\n"
     exit
 #elif
 # Check whether REQUESTFILE argument exists
@@ -17,7 +18,7 @@ fi
 
 MC_CLUSTERNAME="$(echo $MC_APIURL | awk '{split($0, a, "api.");print a[2]}' |awk '{split($0, a, ":");print a[1]}' )" && echo $MC_CLUSTERNAME 
 
-echo Setting up the RHACS sensor on cluster $1
+echo Setting up the RHACS sensor on cluster $MC_CLUSTERNAME
 echo -n "Using rocctl v" 
 roxctl version
 
@@ -28,7 +29,7 @@ export ROX_ENDPOINT="$(oc -n stackrox get route central -o jsonpath="{.status.in
 echo "\$ROX_ENDPOINT: "$ROX_ENDPOINT
 
 # Assuming ROX API Token has been generated before and put to the file roxctl-access-token
-export ROX_API_TOKEN="$(cat ROXCTL_ACCESS_TOKEN_FILE)"
+export ROX_API_TOKEN="$(cat $ROXCTL_ACCESS_TOKEN_FILE)"
 
 # Generate cluster_init_bundle
 #roxctl -e "$ROX_ENDPOINT" central init-bundles generate cluster_init_bundle  --output-secrets cluster_init_bundle.yaml
@@ -37,7 +38,7 @@ export ROX_API_TOKEN="$(cat ROXCTL_ACCESS_TOKEN_FILE)"
 # The secured cluster will read it out (see ACM policy 'policy-advanced-managed-cluster-security')
 # This patch is a customized solution and extends the ootb installation procedure (for the sake of scripting)
 
-yq  '(. | select(.metadata.name == "sensor.tls") | .stringData.acs-host) = "'$ROX_ENDPOINT'"' cluster_init_bundle.yaml > cluster_init_bundle_adjusted.yaml 
+yq  '(. | select(.metadata.name == "sensor.tls") | .stringData.acs-host) = "'$ROX_ENDPOINT'"' $CLUSTER_INIT_BUNDLE_FILE > cluster_init_bundle_adjusted.yaml 
 
 # first login to secured cluster
 oc login --token $MC_APITOKEN --server=$MC_APIURL --insecure-skip-tls-verify=false
