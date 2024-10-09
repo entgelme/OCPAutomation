6017a490c7c1cd268c55d24ba07552b0883d5783
Andreas Entgelmeier <entgelme@de.ibm.com>
2024-10-09 14:10:47 +0200

    setup vault

--- Ansible/ansible-example/setup.sh
+++ Ansible/ansible-example/setup.sh
@@ -7,8 +7,13 @@ import os
 print (os.environ['VAULT_PASSWORD'])
 EOF
 
-echo "Set the Ansible Vault Password in the environment Variable VAULT_PASSWORD"
-#...
+echo "Setting up a new Ansible Vault ..."
+read -p "Enter the password for the new Ansible Vault: " password
+export VAULT_PASSWORD=password
 
+echo "Please enter the key/value pair for 'vault_ocpw: ...'" in the following editor" 
+
+rm group_vars/bastion/vault
 ansible-vault create group_vars/bastion/vault
 
+echo "Done!"
