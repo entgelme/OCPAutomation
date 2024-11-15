283e07e5e4bfad06549a5a50ce2b2e04f69480f9
Andreas Entgelmeier <entgelme@de.ibm.com>
2024-11-15 15:38:14 +0100

    acm-gitops-clusterrole-binding

--- acm-install/install-acm.sh
+++ acm-install/install-acm.sh
@@ -1,2 +1,6 @@
 oc apply -f namespace-acm-hub.yml
 oc apply -f operatorGroup-acm-hub.yml
+oc apply -f subscription-acm-operator.yml 
+sleep 30
+oc apply -f multiclusterhub-multiclusterhub.yml 
+oc get mch -w
