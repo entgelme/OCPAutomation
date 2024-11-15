0ccb11ad2634d41b8f4f6b64a42a9cbee2413756
Andreas Entgelmeier <entgelme@de.ibm.com>
2024-11-15 16:02:33 +0100

    als shell script

--- identity-provider/createHtpasswd.sh
+++ identity-provider/createHtpasswd.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # htpasswd -c -B -b htpasswd workshop-admin <a password>
 oc create secret generic localusers --from-file htpasswd=htpasswd -n openshift-config
 oc adm policy add-cluster-role-to-user cluster-admin workshop-admin
