fae69c3518afcbfc79bfff1e2ac24e4336852c85
Andreas Entgelmeier <entgelme@de.ibm.com>
2024-11-15 15:55:32 +0100

    versch

--- acm-install/install-acm.sh
+++ acm-install/install-acm.sh
@@ -4,3 +4,8 @@ oc apply -f subscription-acm-operator.yml
 sleep 30
 oc apply -f multiclusterhub-multiclusterhub.yml 
 oc get mch -w
+
+# gitops integration
+oc apply -f clusterrole-openshift-gitops-policy-admin.yml 
+oc apply -f clusterrolebinding-openshift-gitops-argocd-application-controller.yml 
+oc apply -f ../acm-gitops/namespace-rhacm-policies.yml 
